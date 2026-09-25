8870f2a49962afc74a70fdd2a44dd53592ef0f30
RISHIIT <prishikess@gmail.com>
2026-09-25 23:58:57 +0530

    Connect frontend to live backend

--- Frontend/hireflow-frontend/src/api/api.js
+++ Frontend/hireflow-frontend/src/api/api.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
-const API = axios.create({
-  baseURL: "http://127.0.0.1:8000/api/",
+const api = axios.create({
+  baseURL: "https://hireflow-zcgh.onrender.com/api",
 });
 
-API.interceptors.request.use((config) => {
+api.interceptors.request.use((config) => {
   const token = localStorage.getItem("access_token");
 
   if (token) {
@@ -15,4 +15,4 @@ API.interceptors.request.use((config) => {
   return config;
 });
 
-export default API;
+export default api;
